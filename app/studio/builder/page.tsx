9e7eaf091f7f88f3bbaab76e7a049e3935492a47
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-05-24 20:05:15 +0600

    add zustand

--- app/studio/builder/page.tsx
+++ app/studio/builder/page.tsx
@@ -4,7 +4,10 @@ import FunctionBuilder from "./components/function-builder";
 export default function BuilderPage() {
     return (
         <>
-            <h1 className="text-4xl font-bold text-center mt-10">FSM Builder Studio</h1>
+            <h1 className="text-4xl font-bold text-center mt-10 p-10 border-2 border-green-500 rounded-md bg-green-500/10 space-x-2 mx-4">
+                <span className="text-green-500">!Welcome To</span>
+                <span >FSM Builder Studio</span>
+            </h1>
             <FunctionBuilder />
         </>
     );
