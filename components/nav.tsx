7d6202b35e5737aeae5c4cd280b3f3ea9d94b7c5
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-05-25 22:38:39 +0600

    theme button changed

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,6 +3,12 @@ import { usePathname } from 'next/navigation'
 import Link from "next/link";
 import ModeToggle from "./theme-button";
 
+const linkSet = [
+    { name: 'Home', href: '/' },
+    { name: 'Builder Studio', href: '/studio/builder' },
+    { name: 'Documentation', href: '/docs' },
+]
+
 export default function Nav() {
     const pathname = usePathname();
 
@@ -10,10 +16,14 @@ export default function Nav() {
         <nav className="bg-transparent text-md p-2 flex justify-between items-center">
             <div className="hover:text-gray-300 font-bold bg-teal-900 py-1 px-4 rounded-md text-white">JudgeFSM</div>
             <div className="flex items-center">
-                <div className="space-x-0.4 rounded-md bg-teal-900 text-white">
-                    <Link href="/" className={`hover:text-gray-300 py-1 px-4 rounded-l-md ${pathname === '/' ? 'bg-teal-400 text-black hover:text-white' : 'bg-teal-900'}`}>Home</Link>
-                    <Link href="/studio/builder" className={`hover:text-gray-300 py-1 px-4 ${pathname === '/studio/builder' ? 'bg-teal-400 text-black hover:text-white' : 'bg-teal-900'}`}>Builder Studio</Link>
-                    <Link href="/docs" className={`hover:text-gray-400 py-1 px-4 rounded-r-md ${pathname === '/docs' ? 'bg-teal-400 text-black hover:text-white' : 'bg-teal-900'}`}>Documentation</Link>
+                <div className="flex space-x-0.5 text-white px-1">
+                    {
+                        linkSet.map((link, i) => (
+                            <Link key={link.href} href={link.href} className={`hover:text-gray-300 px-4 ${pathname === link.href ? 'bg-teal-700 text-black hover:text-white' : 'bg-teal-900'} ${i === 0 ? 'rounded-l-md' : ''} ${i === linkSet.length - 1 ? 'rounded-r-md' : ''}`}>
+                                {link.name}
+                            </Link>
+                        ))
+                    }
                 </div>
                 <ModeToggle />
             </div>
