bb7784d06b945c42796a7ed973b8f09f8e383752
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-06-01 23:56:18 +0600

    add stack

---
+++ app/studio/builder/components/stack/stack.tsx
@@ -0,0 +1,9 @@
+export default function Stack() {
+    return (
+        <div className="p-4 border border-green-700 rounded-md m-4 h-100">
+            <h1 className="text-xl font-bold text-center">
+                Stack
+            </h1>
+        </div>
+    )
+}
--- app/studio/builder/page.tsx
+++ app/studio/builder/page.tsx
@@ -4,14 +4,16 @@ import DFAbuilder from "./components/dfa-builder/dfa-builder";
 import FunctionBuilder from "./components/function-builder/function-builder";
 import TestCaseBuilder from "./components/testcase-builder/testcase-builder";
 import Testing from "./components/testing/testing";
+import Stack from "./components/stack/stack";
 
 export default function BuilderPage() {
     return (
         <>
-            <h1 className="text-2xl font-bold text-center p-5 border-2 border-green-500 rounded-md bg-green-500/10 space-x-2 mx-4">
+            <h1 className="text-2xl font-bold text-center p-5 border border-green-500 rounded-md bg-green-500/5 space-x-2 m-4">
                 <span className="text-green-500">!Welcome To</span>
                 <span >FSM Builder Studio</span>
             </h1>
+            <Stack />
             <Tabs defaultValue="dfa" className="mx-4 mt-5">
                 <TabsList>
                     <TabsTrigger value="dfa">DFA Builder</TabsTrigger>
@@ -19,7 +21,7 @@ export default function BuilderPage() {
                     <TabsTrigger value="testcase">Testcase Builder</TabsTrigger>
                     <TabsTrigger value="testing">Testing</TabsTrigger>
                 </TabsList>
-                <div className="p-2">
+                <div className="p-2 min-h-100">
                     <TabsContent value="dfa"><DFAbuilder /></TabsContent>
                     <TabsContent value="function"><FunctionBuilder /></TabsContent>
                     <TabsContent value="testing"><Testing /></TabsContent>
